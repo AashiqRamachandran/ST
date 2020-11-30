2f0e1a631e1de7ad479fce1d5f75261fff5dd815
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-11-30 21:28:28 +0530

    Update simple_loop_testing.py

--- simple_loop_testing.py
+++ simple_loop_testing.py
@@ -1,23 +1,26 @@
 import os
 
+
 def slt():
     print("Loop size is 5")
-    n=4 #user input
-    ub=n #user input
-    m=n+1 #m is always n+1
+    n = 4  # user input
+    ub = n  # user input
+    m = n + 1  # m is always n+1
 
     if m > n:
-        print("Value is out of bound.")
-    
-    if ub < = n:
+        print("vaues out of bound")
+
+    if n >= ub:
         print("Value is inside bounds and beginning loop testing.")
-        
-        for i in range(0,n): #for  i =0, i<n;i++
-            print("In execution "+str(n))
-            if n==4:
+
+        for i in range(0, n):  # for  i =0, i<n;i++
+            print("In execution " + str(i))
+            if i == 4:
                 print("Time to execute till UB and UB-1")
-        
-        for i in range(0, n): #for i=0;i<n;i++
-            print("In execution "=str(n))
+
+        for i in range(0, n):  # for i=0;i<n;i++
+            print('In execution ' + str(i))
         print("Time to execute till n")
+
+
 slt()
